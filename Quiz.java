c90495fca538a991301f6e26b6db5cbf4b89c37e
KSHITIZ RANJAN <54390873+kshitizranjan15@users.noreply.github.com>
2025-10-09 07:07:06 +0530

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -136,3 +136,4 @@ public class Quiz extends JFrame implements ActionListener{
 }
 // This quiz application can be enhanced more.
 // Apart from radio select buttons we should provide maulti select option also.
+// Gor Ui instead of swing now we can use modern Frameworks like react.
